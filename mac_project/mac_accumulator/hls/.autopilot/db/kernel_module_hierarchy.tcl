6ad184a844c1c95b698acc369f728f25add21436
Prince Baah-Mensah <155831606+Baah134@users.noreply.github.com>
2026-07-26 02:29:48 +0000

    Delete mac_project directory

--- mac_project/mac_accumulator/hls/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,5 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "mac_accumulator", "RefName" : "mac_accumulator","ID" : "0","Type" : "pipeline",
-"SubLoops" : [
-	{"Name" : "VITIS_LOOP_22_1","RefName" : "VITIS_LOOP_22_1","ID" : "1","Type" : "pipeline"},]
-}]}
